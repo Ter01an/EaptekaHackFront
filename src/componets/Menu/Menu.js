e0d384c07fdd51fc5f9b71a263dc0b8a133ead84
ter01an <7913546825>
2021-05-30 11:45:24 +0600

    Menu icons

--- src/componets/Menu/Menu.js
+++ src/componets/Menu/Menu.js
@@ -1,6 +1,12 @@
 import "./Menu.scss";
 import { Link } from "wouter";
 
+import imgCart from "./static/icon-cart.svg";
+import imgCatalog from "./static/icon-catalog.svg";
+import imgHome from "./static/icon-home.svg";
+import imgSber from "./static/icon-sber.png";
+import imgUser from "./static/icon-user.svg";
+
 export default function Menu() {
 
     function handleChatOpen() {
@@ -14,33 +20,40 @@ export default function Menu() {
     return (
         <div className="Menu">
             <Link href="/" className="Menu__item" id="justwidget--asst-close" onClick={handleChatClose}>
-                <div className="Menu__icon"/>
+                <div className="Menu__icon">
+                    <img src={imgHome} alt="Главный"/>
+                </div>
                 <div className="Menu__title">
                     Главная
                 </div>
             </Link>
             <Link href="/" className="Menu__item" onClick={handleChatClose}>
-                <div className="Menu__icon"/>
+                <div className="Menu__icon">
+                    <img src={imgCatalog} alt="Каталог"/>
+                </div>
                 <div className="Menu__title">
-                    Label
+                    Каталог
                 </div>
             </Link>
             <div className="Menu__item" id="justwidget--asst-button" onClick={handleChatOpen}>
-                <div className="Menu__icon"/>
-                <div className="Menu__title">
-                    Ассистент
+                <div className="Menu__icon Menu__icon--sber">
+                    <img src={imgSber} alt="Ассистент"/>
                 </div>
             </div>
             <Link href="/" className="Menu__item" onClick={handleChatClose}>
-                <div className="Menu__icon"/>
+                <div className="Menu__icon">
+                    <img src={imgCart} alt="Корзина"/>
+                </div>
                 <div className="Menu__title">
-                    Label
+                    Корзина
                 </div>
             </Link>
             <Link href="/" className="Menu__item" onClick={handleChatClose}>
-                <div className="Menu__icon"/>
+                <div className="Menu__icon">
+                    <img src={imgUser} alt="Профиль"/>
+                </div>
                 <div className="Menu__title">
-                    Label
+                    Профиль
                 </div>
             </Link>
         </div>
